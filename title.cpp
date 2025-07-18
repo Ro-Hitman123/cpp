7f88931488e9a123be884678fff6290f77bb9101
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-07-18 13:08:53 +0545

    first commit

---
+++ title.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    int a=5, b=2;
+
+    cout << "The value of a + b is" << a+b<<endl ;
+    cout << "The value of a - b is" << a-b<<endl ;
+    cout << "The value of a/b is" << a/b <<endl;
+    cout << "The value of a * b is" << a*b<<endl ;
+    cout << "The value of a++ is" << a++<<endl;
+    cout << "The value of ++a is" << ++a<<endl;   
+    cout << "The value of a-- is" << a--<<endl;
+    cout << "The value of --a is" << --a<<endl;
+    
+    
+    
+
+   return 0;
+
+}
