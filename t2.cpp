3a60f60af9e658ec410856b7f939d45e6278bd9e
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-07-20 18:59:24 +0545

    initial commit

--- t2.cpp
+++ t2.cpp
@@ -28,13 +28,13 @@ int main() {
     cout << "Before swap: the value of x is: " << x << ", the value of y is: " << y << endl;
     swap(&x, &y);
     cout << "After swap: the value of x is: " << x << ", and the value of y is: " << y << endl; 
-    cout << "Before swap: the value of x is: " << x << ", the value of y is: " << y << endl;
-    swapVar(x, y);
-    cout << "After swap: the value of x is: " << x << ", and the value of y is: " << y << endl;
-    return 0;
+    // cout << "Before swap: the value of x is: " << x << ", the value of y is: " << y << endl;
+    // swapVar(x, y);
+    // cout << "After swap: the value of x is: " << x << ", and the value of y is: " << y << endl;
+    // return 0;
 
-    cout << "Before swap: the value of x is: " << x << ", the value of y is: " << y << endl;
-    swapVar(x, y) = 100; // This will change the value of x to 100
-    cout << "After swap: the value of x is: " << x << ", and the value of y is: " << y << endl;
+    // cout << "Before swap: the value of x is: " << x << ", the value of y is: " << y << endl;
+    // swapVar(x, y) = 100; // This will change the value of x to 100
+    // cout << "After swap: the value of x is: " << x << ", and the value of y is: " << y << endl;
     return 0;
 }
---
+++ t4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int factorial (int n){
+    if (n<=1){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+
+
+// The fibonacci series is a sequence where each number is the sum of the two preceding ones, usually starting with 0 and 1.
+// The series starts like this: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+int fib(int n){
+    if(n<2){
+        return 1;
+    }
+    return fib(n-2) + fib(n-1);
+}
+
+int main() {
+    int a;
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+    cout<<"The factorial of "<< a << " is " << factorial(a) << endl; 
+    // cout<<"The term in fibonacci sequence at position "<<a<< " is "<<fib(a)<<endl;
+    return 0;
+}
+
+
