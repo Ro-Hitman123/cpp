763ad860e1a0427ca61c59b5031af0ff73e41b34
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-07-30 21:56:47 +0545

    initial

--- t31.cpp
+++ t31.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class {
-
+class hello{
+public:
+    void displaly(){
+        cout<<"Hello World";
+    }
 };
 
 int main() {
-
+    hello c;
+    c.displaly();
     return 0;
 }
