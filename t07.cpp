d34db7cd4529dd156db711cc0c5cafbaa90549c7
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-08-13 18:23:53 +0545

    initial commit

--- t07.cpp
+++ t07.cpp
@@ -9,7 +9,7 @@ class Name{
 class District:public Name{
 	public:
 		void show(){
-			cout<<"My District Name is Kapilvastuuuuuuuuuuuu"<<endl;
+			cout<<"My District Name is Kapilvastuuuuuuuuuuue"<<endl;
 		}
 };
 class Campus:public Name{
