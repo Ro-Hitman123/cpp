efe6bf4643fffc9e9c15f16ef1c6800cdc723448
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-08-15 20:20:34 +0545

    fljhjkfh

--- t07.cpp
+++ t07.cpp
@@ -9,7 +9,7 @@ class Name{
 class District:public Name{
 	public:
 		void show(){
-			cout<<"My District Name is Kapilvastuuuuuuuuuug"<<endl;
+			cout<<"My District Name is Kapilvastuuuuuuuuuugjosfls"<<endl;
 		}
 };
 class Campus:public Name{
