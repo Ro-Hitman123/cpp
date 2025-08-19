6caae429cd2031324750470165dbe4ff6815ee9c
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-08-19 20:09:00 +0545

    jgf

---
+++ t00.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+    int id;
+    string name; // Add name member
+public:
+    Student(){
+        cout << "Enter student name: ";
+        cin >> name;
+        cout << "Enter id: ";
+        cin >> id;
+    }
+    void Studentdisplay(){
+        cout << "Student Name: " << name << endl;
+        cout << "The ID of the student is: " << id << endl;
+    }
+};
+
+class Mark{
+    int mark;
+public:
+    Mark(){
+        cout << "Enter mark: ";
+        cin >> mark;
+    }
+    friend class Result;
+    void Markdisplay(){
+        cout << "The Mark of the student is: " << mark << endl;
+    }
+};
+
+class Result:public Student, public Mark{
+    int result;
+public:
+    Result(){
+        result = mark;
+    }
+    void display(){
+        Student::Studentdisplay();
+        Mark::Markdisplay();
+        cout << "The Result of the student is: " << result << endl;
+    }
+};
+
+int main() {
+    const int n = 10;
+    Result students[n]; // Array of 10 students
+
+    for (int i = 0; i < n; i++) {
+        cout << "\nDetails for student " << i + 1 << ":" << endl;
+        students[i].display();
+    }
+    return 0;
+}
