244bd8193798147c17c1c770f775f3e9aaf9ca0d
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-08-28 22:24:16 +0545

    fdjh

--- t41 Template overloading.cpp
+++ t41 Template overloading.cpp
@@ -12,6 +12,34 @@ void func(T a){
 
 int main()
 {
-    func(4);  //Exact match takes the highest priority
+    func(4);  //Exact match takes the highest priorit
     return 0;
 }
+
+//Member function
+/*template <class T>
+class Harry
+{
+public:
+    T data;
+    Harry(T a)
+    {
+        data = a;
+    }
+    void display();
+};
+ 
+template <class T>
+void Harry<T> :: display(){
+    cout<<data;
+}
+
+
+int main()
+{
+    Harry<int> h(5.7);
+    cout << h.data << endl;
+    h.display();
+    return 0;
+}
+*/
