39cebd5b13e8894f371cc34f2981d03fc4366f1c
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-08-30 23:43:00 +0545

    sdf

--- t42.cpp
+++ t42.cpp
@@ -7,30 +7,17 @@ private:
     int minutes;
 
 public:
-    // Default constructor
-    TIME() : hour(0), minutes(0) {}
-
-    // Parameterized constructor
+    // Constructor using member initializer list
     TIME(int h, int m) : hour(h), minutes(m) {}
 
-    // Conversion constructor: converts int (total minutes) to TIME
-    TIME(int totalMinutes) {
-        hour = totalMinutes / 60;
-        minutes = totalMinutes % 60;
-    }
-
-    void displayTime() const {
+    void displayTime() {
         cout << "Time: " << hour << " hour(s) and " << minutes << " minute(s)" << endl;
     }
 };
 
 int main() {
-    int totalMinutes = 135;  // example total minutes
-
-    // Convert integer to TIME object using conversion constructor
-    TIME t = totalMinutes;
-
-    t.displayTime();  // Output: Time: 2 hour(s) and 15 minute(s)
+    TIME t(2, 45);   // Initialize TIME object to 2 hours 45 minutes
+    t.displayTime();
 
     return 0;
 }
